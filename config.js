8d57e2f2ffa906bb952fd2d5f61ea2179ca88e9f
Lorenzo Jimenez <lorenzojimenez101@yahoo.com>
2018-11-27 10:13:53 -0700

    refactor database url

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.DATABASE_URL =
     process.env.DATABASE_URL ||
     global.DATABASE_URL ||
-    'mongodb://Lorejimenez1:Pregamelore11@ds019638.mlab.com:19638/adoptionevents';
+    'mongodb://localhost/adoptionevents';
 exports.PORT = process.env.PORT || 8080;
